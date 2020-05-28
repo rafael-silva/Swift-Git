4c9cf05435ebc130954b897affbba7ff099aaf8a
Rafael de Matos da Silva <rafael_torrica@hotmail.com>
2020-05-27 21:01:13 -0300

    Entregas 	Pull to refresh. 		Feito um refactor no view model para externalizar a chamada da api, para então realizar a função de refresh.

--- Swift-Git/Swift-Git/Features/Search Repository/ViewModel/RepositoriesViewModel.swift
+++ Swift-Git/Swift-Git/Features/Search Repository/ViewModel/RepositoriesViewModel.swift
@@ -1,14 +1,22 @@
 protocol RepositoriesViewModelProtocol {
     var repositories: Dynamic<[Item]?> { get }
     var error: Dynamic<String?> { get }
+    
+    func loadRepositories()
 }
 
 final class RepositoriesViewModel: RepositoriesViewModelProtocol {
     
     private(set) var repositories: Dynamic<[Item]?> = Dynamic(nil)
     private(set) var error: Dynamic<String?> = Dynamic(nil)
-
+    private let api: APIRepositoryProviderProtocol
+    
     init(api: APIRepositoryProviderProtocol = API()) {
+        self.api = api
+        loadRepositories()
+    }
+    
+    func loadRepositories() {
         api.repositories(language: "swift", sort: "stars") { response in
             switch response {
             case.success(let output):
